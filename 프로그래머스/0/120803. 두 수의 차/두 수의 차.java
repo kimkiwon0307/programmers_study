3fa2c29f43f8c475a0af01e28add1d39ffd25bd0
kimkiwon <46276548+kimkiwon0307@users.noreply.github.com>
2024-03-28 19:01:00 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
@@ -1,6 +1,10 @@
 class Solution {
     public int solution(int num1, int num2) {
-       
-        return num1 - num2;
+      
+              
+        int answer = num1 - num2;
+                
+    
+        return answer;
     }
 }
