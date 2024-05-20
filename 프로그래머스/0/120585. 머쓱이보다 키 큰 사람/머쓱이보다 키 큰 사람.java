e10b3eec3587aaa00485b2ac2947c498bec9a110
kimkiwon <46276548+kimkiwon0307@users.noreply.github.com>
2024-05-20 11:33:21 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] array, int height) {
+       
+        int count = 0;
+        
+        for(int i=0; i<array.length; i++){
+            
+            if(height < array[i]){
+                count++;
+            }
+            
+            
+        }
+        
+        int answer = count;
+        return answer;
+    }
+}
