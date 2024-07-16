73e4f2d25a9ae3f36b04c7f9e12b078bebb86081
kimkiwon <46276548+kimkiwon0307@users.noreply.github.com>
2024-07-16 20:24:59 +0900

    [level 0] Title: 짝수의 합, Time: 0.06 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,29 +1,18 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+       
         int sum = 0;
         
-        
-        for (int i = 0; i <= n; i ++){
+        for(int i=0; i<=n; i++){
             
-            if(i == n){
-                
-                for(int j = 0; j<= i; j++){
-                    
-                    if(j % 2 == 0){
-                        answer += j;
-                    }
-                    
-                }
-                
-                
+            if(i % 2 == 0){
+                sum += i;
             }
-            
-            
             
         }
         
         
+        int answer = sum;
         return answer;
     }
 }
