780b4228d373f9bb628db17693243694d70cd21c
kimkiwon <46276548+kimkiwon0307@users.noreply.github.com>
2024-07-18 16:39:49 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,20 +1,16 @@
+
 class Solution {
     public int solution(int price) {
-        double answer = 0;
-       
-        if(price < 100000){
-            answer = (double)price;
+        int answer = 0;
+        if(100000 <= price && price < 300000){
+            answer = (int) (price * 0.95);
+        }else if(300000 <= price && price < 500000){
+            answer = (int) (price * 0.9);
+        }else if(500000 <= price){
+            answer = (int) (price * 0.8);
+        }else{
+            answer = price;
         }
-        else if(price >= 100000 && price < 300000){
-          answer =  price - (price * 0.05);
-        }else if(price >= 300000 && price < 500000){
-           answer =  price - (price * 0.1);
-        }else if( price >= 500000){
-           answer =  price - (price * 0.2);
-        }
-        
-        
-        
-        return (int)answer;
+        return answer;
     }
 }
