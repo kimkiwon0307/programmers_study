67142124b3be887ccfb3b62923b6ad8ff2bc5f55
kimkiwon <46276548+kimkiwon0307@users.noreply.github.com>
2024-07-19 18:46:29 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,16 +1,11 @@
 class Solution {
     public int[] solution(int money) {
-        int jan = 0;
-        int janMoney = 0;
-        jan = money / 5500;
-        janMoney = money % 5500;
-        
-        int[] answer = new int[2];
-        
-        answer[0] = jan;
-        answer[1] = janMoney;
+      
+        int americano = money / 5500;
+        int nammoney = money -(americano * 5500);
         
         
+        int[] answer = new int[] {americano, nammoney};
         return answer;
     }
 }
